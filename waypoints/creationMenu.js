a0a164ad951e9348ceaf2a041c28644a02b8a55a
ThePoptartCrpr <ThePoptartCrpr@users.noreply.github.com>
2018-11-29 19:38:11 -0800

    More GUI additions + early icons

--- waypoints/creationMenu.js
+++ waypoints/creationMenu.js
@@ -1,7 +1,7 @@
 // Variables
 var wp_waypointGui = new Gui();
 var wp_bgAlpha, wp_fgAlpha, wp_guiDepth, wp_waypointGuiElements;
-var wp_waypointGuiGap = 50;
+var wp_waypointGuiGap = 35;
 
 // Initializing and opening
 register('command', 'wp_openWaypointCreationGui').setName('createWaypoint');
@@ -14,7 +14,8 @@ function wp_openWaypointCreationGui() {
   // Add elements
   wp_waypointGuiElements.push(new wp_waypointTitleElement('Create Waypoint'));
   wp_waypointGuiElements.push(new wp_waypointInvisElement());
-  wp_waypointGuiElements.push(new wp_waypointTextElement('Name'));
+  wp_waypointGuiElements.push(new wp_waypointTextElement('Name:'));
+  wp_waypointGuiElements.push(new wp_waypointColorElement('Color'));
 
   // Open
   wp_waypointGui.open();
@@ -27,7 +28,7 @@ wp_waypointGui.registerClicked(function() {
 register('step', function() {
   if (wp_waypointGui.isOpen()) {
     wp_bgAlpha = easeOut(wp_bgAlpha, 150, 10);
-    wp_fgAlpha = easeOut(wp_fgAlpha, 100, 12);
+    wp_fgAlpha = easeOut(wp_fgAlpha, 100, 13);
 
     // Step elements
     wp_waypointGuiElements.forEach(function(element) {
@@ -51,8 +52,9 @@ wp_waypointGui.registerDraw(function(mouseX, mouseY) {
     if (element instanceof wp_waypointInvisElement) Renderer.drawRect(Renderer.color(0, 0, 0, wp_fgAlpha), 50, y - 20, Renderer.screen.getWidth() - 100, wp_guiDepth - y);
   });
 
-  if (!wp_guiDepth) wp_guiDepth = y + 5;
-  wp_guiDepth = easeOut(wp_guiDepth, y, 3);
+  wp_guiDepth = y;
+  // if (!wp_guiDepth) wp_guiDepth = y;
+  // wp_guiDepth = easeOut(wp_guiDepth, y, 4);
 });
 
 
@@ -155,5 +157,83 @@ function wp_waypointTextElement(desc) {
     ).setShadow(true).draw();
 
     return y + wp_waypointGuiGap;
+  }
+}
+
+function wp_waypointColorElement(title) {
+  this.color = {
+    r: 255,
+    g: 255,
+    b: 255
+  };
+  this.desc = title;
+  this.alpha = 0;
+
+  this.step = function() {
+    this.alpha = easeOut(this.alpha, 255, 10);
+  }
+
+  // Text
+  this.draw = function(x, y, mouseX, mouseY) {
+    Renderer.text(
+      this.desc,
+      110,
+      y
+    ).setColor(
+      Renderer.color(
+        255,
+        255,
+        255,
+        this.alpha
+      )
+    ).setShadow(true).draw();
+
+    // Red selector
+    Renderer.drawRect(
+      Renderer.color(this.color.r, 0, 0, this.alpha),
+      100,
+      y + 20,
+      (Renderer.screen.getWidth() / 2) - 170,
+      3
+    );
+
+    // Green selector
+    Renderer.drawRect(
+      Renderer.color(0, this.color.g, 0, this.alpha),
+      100,
+      y + 40,
+      (Renderer.screen.getWidth() / 2) - 170,
+      3
+    );
+
+    // Blue selector
+    Renderer.drawRect(
+      Renderer.color(0, 0, this.color.b, this.alpha),
+      100,
+      y + 60,
+      (Renderer.screen.getWidth() / 2) - 170,
+      3
+    );
+
+    // Color preview
+    // Black outline
+    Renderer.drawRect(
+      Renderer.color(0, 0, 0, this.alpha),
+      (Renderer.screen.getWidth() / 2) - 60,
+      y + 15,
+      53,
+      53
+    );
+
+    // Color fill
+    Renderer.drawRect(
+      Renderer.color(this.color.r, this.color.g, this.color.b, this.alpha),
+      (Renderer.screen.getWidth() / 2) - 57,
+      y + 18,
+      47,
+      47
+    )
+
+    return y + 140;
   }
 }
